c2b57a4fa8ee09acd78ab7c0f34edb065725c25a
biersdorfer-jon <jbiersdorfer02@gmail.com>
2023-12-19 02:08:31 -0600

    First

--- src/HeroBgAnimation/HeroBgAnimationStyle.js
+++ src/HeroBgAnimation/HeroBgAnimationStyle.js
@@ -2,6 +2,17 @@ import styled from 'styled-components';
 
 
 export const Div = styled.div`
-    width:600px;
+    width:500px;
     height: 500px;
+    padding: 100px 0px;
+
+    @media only screen and (max-width: 960px) {
+        padding: 0px 0px 80px 0px;
+    }
+
+    @media only screen and (max-width: 648px) {
+        padding: 30px 0px;
+        width: 350px;
+        height: 350px;
+    }
 `
